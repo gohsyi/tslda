8e5b93cc901fd1eed618565ab881fc55b0bcacd1
Henry Guo <guohongyi@sjtu.edu.cn>
2021-02-21 10:58:05 +0800

    Update tslda.py

--- tslda.py
+++ tslda.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import numpy as np  # for np.random.dirichlet(alpha, size)
+import numpy as np
 
 
 class TSLDA:
@@ -12,6 +12,8 @@ class TSLDA:
         self.n_topics = K
         self.n_sentiments = S
         self.counter = defaultdict(lambda: 0)  # key: (sentence, topic, sentiment, word, category)
+        self.group_by_topic = [dict() for _ in range(self.n_topics)]
+        self.group_by_sentiment = [dict() for _ in range(self.n_sentiments)]
         self.pretrain()
 
     def pretrain(self):
@@ -22,7 +24,21 @@ class TSLDA:
         for doc in self.documents:
             for sent in doc.sentences:
                 # sent.topic, sent.sentiment = multinomial()
-                pass
+                p = np.zeros((self.n_topics, self.n_sentiments))
+                for a in range(self.n_topics):
+                    for b in range(self.n_sentiments):
+                        Za = len(self.group_by_topic[a]) - int(sent.topic == a)
+                        Zb = len(self.group_by_sentiment[b]) - int(sent.sentiment == b)
+                        # TODO
+                        p[a, b] = (Za + self.beta) * (Zb + self.gamma) * () / () * () / ()
+                if sent.topic is not None:
+                    self.group_by_topic[sent.topic].pop(sent)
+                if sent.sentiment is not None:
+                    self.group_by_sentiment[sent.sentiment].pop(sent)
+                p /= p.sum()
+                ind = np.random.multinomial(1, p.flatten()).argmax()
+                sent.topic = ind // self.n_sentiments
+                sent.sentiment = ind % self.n_sentiments
 
     def __call__(self, documents):
         weights = np.zeros((self.n_topics, self.n_sentiments))
